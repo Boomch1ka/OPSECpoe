7afea52e5d9c1aa3ca3da52a22d02502d8ad01aa
Boomch1ka <131256950+Boomch1ka@users.noreply.github.com>
2025-06-26 22:14:17 +0200

    Delete src directory

--- src/main/java/com/example/budget_app/data/CategorySpendingSummary.kt
+++
@@ -1,8 +0,0 @@
-package com.example.budget_app.data
-
-import androidx.room.ColumnInfo
-
-data class CategorySpendingSummary(
-    @ColumnInfo(name = "category_id") val categoryId: Long,
-    @ColumnInfo(name = "totalSpent") val totalSpent: Double
-)
